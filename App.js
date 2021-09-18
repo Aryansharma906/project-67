8ccf226a76556f91eee522959dbc4ba0688cebf7
Aryansharma906 <aryanaryan.9956@gmail.com>
2021-09-18 16:17:56 +0530

    project 67
    Project 67

--- App.js
+++ App.js
@@ -1,15 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import  * as React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import {Fb} from './Fb';
+import  {Inst} from './Inst';
 
-export default function App() {
+export default class App extends React.Component() {
+  render() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AppContainer/>
   );
+  }
 }
+
+const BottomTabNavigation = createBottomTabNavigator({
+  Facebook: {screen: Fb },
+  Instagram: {screen: Inst}
+})
+
+const AppContainer = createAppContainer(BottomTabNavigation);
 
 const styles = StyleSheet.create({
   container: {
---
+++ Fb.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
+
+export default class Fb extends React.Component{
+     render(){
+          return(
+               <View>Facebook</View>
+
+          );
+     };
+}
